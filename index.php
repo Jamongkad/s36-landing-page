64b219626dfe8f2e87670f92a7ad1a6fe36b5a3b
mathew <mathew@ubuntu.(none)>
2012-09-19 07:33:42 +0800

    the seabitch

--- index.php
+++ index.php
@@ -4,14 +4,19 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim(array(
     'templates.path' => './views'
+  , 'mode' => 'development'
 ));
 
 $app->get('/', function() use ($app) {
-    $app->render('my_index.php'); 
+    $req = $app->request();
+    $param = $req->get();
+    $app->render('my_index.php', array('id' => 'mathew')); 
 });
 
-$app->get('/hello/:name', function($name) use ($app) {
-    echo "Hello, $name";
+$app->post('/add_email', function() use ($app) { 
+    $req = $app->request();
+    $param = $req->post();
+    print_r($param); 
 });
 
 $app->run();
